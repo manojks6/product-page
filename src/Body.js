91f4e240e9e4eb9af5ecedb88a52fa941689585e
manojks6 <manumanojks600@gmail.com>
2024-01-03 22:37:55 +0530

    1.4{ Product Name alignment}

--- src/Body.js
+++ src/Body.js
@@ -12,18 +12,18 @@ function Body(){
                <div className="Items"><Product name='Combo Pack Of 2 Casual Shoes Sneakers For Men(Black)' price='$895' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/d/z/z/10-combo-p-9004-1304-10-bersache-multicolor-original-imagpjvy9gs5xfza.jpeg?q=70'/> </div>
                </div>
                <div className='row'>
-               <div className="Items"><Product name='High Ankle Riding Boots for Hiking & Mild Waterproof Protection Boots For Men  (Olive, Green)' price='$548' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/2/g/z/11-s-power-black-11-unistar-black-original-imagupq69bymvhvc.jpeg?q=70'/> </div>
+               <div className="Items"><Product name='High Ankle Riding Boots for Hiking & Mild Waterproof Protection Boots For Men' price='$548' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/2/g/z/11-s-power-black-11-unistar-black-original-imagupq69bymvhvc.jpeg?q=70'/> </div>
                <div className="Items"><Product name='Lescon Airfoam Sapphire 2 Füme Erkek Spor Ayakkabı Airfoam Sapphire 2 22NAE00AISPM' price='$555' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/4/p/4/-original-imagpzm9apyqghyx.jpeg?q=70'/> </div>
-               <div className="Items"><Product name=' Combo of 2,Stylish Lightweight Casual Lace Sneaker for Mens & Girls Sneakers For Men Sneakers For Men ' price='$869' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/u/v/q/8-combo-pp-a-1243-1304-bersache-multicolor-original-imagprxh6tyd9gje.jpeg?q=70'/> </div>
+               <div className="Items"><Product name='Combo of 2,Stylish Lightweight Casual Lace Sneaker for Mens & Girls Sneakers  ' price='$869' image='https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/u/v/q/8-combo-pp-a-1243-1304-bersache-multicolor-original-imagprxh6tyd9gje.jpeg?q=70'/> </div>
                <div className="Items"><Product name="Stussy Inspired Hoodies, Streetwear Sweatshirts, Women's Stussy, Unisex Casual" price='$758' image='https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/3/d/i/l-jc22-hd-fs-black-jmc-pckt-jump-cuts-original-imagg3yxfk48y7nt.jpeg?q=70'/> </div>
-               <div className="Items"><Product name="Stussy Inspired Hoodies, Streetwear Sweatshirts, Casual Tops, Men's" price='$258' image='https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/u/z/v/xl-all-rbcpon-sky-one-nb-nicky-boy-original-imagrdhka9mzrn5d.jpeg?q=70'/> </div>
+               <div className="Items"><Product name="Stussy Inspired Hoodies,Streetwear Sweatshirts, Casual Tops, Men's (Black color)" price='$258' image='https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/u/z/v/xl-all-rbcpon-sky-one-nb-nicky-boy-original-imagrdhka9mzrn5d.jpeg?q=70'/> </div>
                </div>
                <div className='row'>
                <div className="Items"><Product name="Red Tape Kid's Rover Stylish Clogs  Indoor & Outdoor Sandals Clogs for Kids" price='$854' image='https://rukminim2.flixcart.com/image/612/612/xif0q/kids-sandal/m/z/l/3-rtkidskbrv-red-tape-original-imagqygqka8me93r.jpeg?q=70'/> </div>
-               <div className="Items"><Product name="Xiaomi 43 Inch TV UHD Smart android TV - ELA4378GL" price='$1250' image='https://rukminim2.flixcart.com/image/312/312/xif0q/television/q/9/l/-original-imagu4364qh4huyj.jpeg?q=70'/> </div>
-               <div className="Items"><Product name='HP x5600B 64GB OTG (Type B) usb3.2 Pen Drive, Grey & Black' price='$258' image='https://rukminim2.flixcart.com/image/612/612/xif0q/pendrive/pendrive/t/x/b/v220z-hp-original-imagtvwnefs4vxpc.jpeg?q=70'/> </div>
+               <div className="Items"><Product name="Xiaomi 43 Inch TV UHD Smart android TV - ELA4378GLOJK(4K Supported)" price='$1250' image='https://rukminim2.flixcart.com/image/312/312/xif0q/television/q/9/l/-original-imagu4364qh4huyj.jpeg?q=70'/> </div>
+               <div className="Items"><Product name='HP x5600B 128GB OTG (Type B) usb3.2 Pen Drive, Grey & Black ' price='$258' image='https://rukminim2.flixcart.com/image/612/612/xif0q/pendrive/pendrive/t/x/b/v220z-hp-original-imagtvwnefs4vxpc.jpeg?q=70'/> </div>
                <div className="Items"><Product name='BEST Wired Optical Mechanical Gaming Keyboard & Mouse Rainbow RGB Backlit Combo' price='$547' image='https://i.ebayimg.com/images/g/ZsEAAOSwipleYsuJ/s-l1600.jpg'/> </div>
-               <div className="Items"><Product name='Men Hoodie Casual Spring Street Hoodie Sweatshirt' price='$257' image='https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/b/f/0/l-ausk0622-ausk-original-imagvfpfszrgzzar.jpeg?q=70'/> </div>
+               <div className="Items"><Product name='Men Hoodie Casual Spring Street White Hoodie Sweatshirt' price='$257' image='https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/b/f/0/l-ausk0622-ausk-original-imagvfpfszrgzzar.jpeg?q=70'/> </div>
                </div>
           </div>
      )
